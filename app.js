44439b08ab989b35d23f46afeaeb917fb0e5fa1f
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-04-26 23:44:23 +0100

    the soll

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const formData = new FormData(form);
+            const name = formData.get('name');
+            const author_email = formData.get('author_email');
+            const topic_title = formData.get('topic_title');
+            const target_level = formData.get('target_level');
+            const topic_details = formData.get('topic_details');
+            const expected_result = formData.get('expected_result');
+            const obj = {
+                name,
+                author_email,
+                topic_details,
+                topic_title,
+                target_level,
+                expected_result,
+            }
+            console.log(obj)
+            // axios.post('http://localhost:7777/video-request', obj);
+        })
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const name = formData.get('name');
-        const author_email = formData.get('author_email');
-        const topic_title = formData.get('topic_title');
-        const target_level = formData.get('target_level');
-        const topic_details = formData.get('topic_details');
-        const expected_result = formData.get('expected_result');
-        const obj = {
-            name,
-            author_email,
-            topic_details,
-            topic_title,
-            target_level,
-            expected_result,
-        }
-        console.log(obj);
-        // axios.post('', obj);
-    })
+})
